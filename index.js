572d71e5888f18adaca23079fc6a8120b8095404
Usman Abdur Rehman <saidskit@outlook.com>
2024-01-11 14:22:53 +0500

    SwitchTrue code added

--- index.js
+++ index.js
@@ -1,60 +1,23 @@
-// "https://jsonplaceholder.typicode.com/posts"
+class Junior {}
 
-/*
-Array.fromAsync
+class Senior {}
 
-Used for 
-Async Iterators => Async Generators / ReadableStreams
-Sync Interators => Set, Map, 
-Array like objects => objects with a length property and indexed elements
-*/
+class Associate {}
 
-const getPosts = async () => {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
+const employee = new Junior({});
 
-  return data;
-};
+if (employee instanceof Junior) {
+}
+if (employee instanceof Senior) {
+}
+if (employee instanceof Associate) {
+}
 
-(async () => {
-  // Async Iterators
-  const genFn = async function* () {
-    const data = await getPosts();
-    yield data;
-  };
-  const gen = genFn();
-  const asyncIter = await Array.fromAsync(gen);
-
-  // Sync Iterators
-  const syncIter = await Array.fromAsync(new Set([1, 2, 3]));
-  const syncIterPromise = await Array.fromAsync(
-    new Set([
-      new Promise(async (resolve) => {
-        const posts = await getPosts();
-        resolve(posts);
-      }),
-    ])
-  );
-
-  // Array like objects
-  const syncObject = await Array.fromAsync({
-    length: 1,
-    0: "Apples",
-  });
-  const syncObjectPromise = await Array.fromAsync({
-    length: 1,
-    0: new Promise(async (resolve) => {
-      const posts = await getPosts();
-      resolve(posts);
-    }),
-  });
-
-  console.log({
-    asyncIter,
-    syncIter,
-    syncIterPromise,
-    syncObject,
-    syncObjectPromise,
-  });
-})();
+switch (true) {
+  case employee instanceof Junior: {
+  }
+  case employee instanceof Senior: {
+  }
+  case employee instanceof Associate: {
+  }
+}
